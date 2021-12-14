3155f1c2acfb5d1cfc46fecb63488937324e2694
Youded-byte <82912625+Youded-byte@users.noreply.github.com>
2021-12-14 21:52:38 +0100

    Support for command line arguments

--- Source.cpp
+++ Source.cpp
@@ -17,14 +17,14 @@ DWORD __stdcall readDataFromExtProgram(HANDLE m_hChildStd_OUT_Rd)
 		for (int i = 0;i < 20000;i++) {
 			if (chBuf[i] == 13 && chBuf[i + 1] == 10) {
 				chBuf[i] = 10;
-				for (int it = i + 1; it < 2002 - i; it++) {
+				for (int it = i + 1; it < 19999 - i; it++) {
 					chBuf[it] = chBuf[it + 1];
 				}
 			}
 		}
 
 		std::cout << chBuf;
-		ZeroMemory(chBuf, 128);
+		ZeroMemory(chBuf, 20000);
 
 		if (!bSuccess) break;
 	}
@@ -33,7 +33,7 @@ DWORD __stdcall readDataFromExtProgram(HANDLE m_hChildStd_OUT_Rd)
 
 
 
-int main() {
+int main(int argc, char** argv) {
 	STARTUPINFO si;
 	PROCESS_INFORMATION pi;
 	ZeroMemory(&si, sizeof(si));
@@ -64,15 +64,36 @@ int main() {
 		//std::cout << "Couldn't set handle information!";
 		return 0;
 	}
+	BOOL findstr = TRUE;
+	wchar_t tasklistargs[8192] = L"";
+	ZeroMemory(&tasklistargs, sizeof(tasklistargs));
+	if (argc > 1) {
+		for (int i = 1; i < argc; i++) {
+			wchar_t tasklistargstemp[8192] = L"";
+			size_t size = strlen(argv[i]);
+			mbstowcs(tasklistargstemp, argv[i], size);
+			wcsncat(tasklistargs, tasklistargstemp, wcslen(tasklistargstemp));
+			wcsncat(tasklistargs, L" ", 1);
+		}
+		findstr = FALSE;
+	}
 
 	si.hStdError = m_hChildStd_OUT_Wr;
 	si.hStdOutput = m_hChildStd_OUT_Wr;
 	si.dwFlags |= STARTF_USESTDHANDLES;
 	wchar_t lpCommandLine[8192] = L"";
-	wchar_t args[8192];
-	wcscpy(args, L"\\System32\\cmd.exe /c tasklist || findstr /I 'lunar javaw chrome firefox cmd conhost tasklist wmiprvse powershell runtimebroker shellexperiencehost svchost useroobebroker systemsettings calculator applicationframehost steam steamwebhelper discord nvidia textinputhost security lockapp task win csrss searchindexer appvsh office mpcopy sgrm vmmem msmpeng nvtelemetry amd intel nvdisplay fontdrv lsass lsaiso services smss registry secure system idle process image name brave opera ===='");
-	wcsncat(lpCommandLine, windir , (size_t)4096);
-	wcsncat(lpCommandLine, args, (size_t)4096);
+	wchar_t cmd[] = L"\\System32\\cmd.exe /c  ";
+	wchar_t tasklist[] = L"\\System32\\tasklist.exe ";
+	wchar_t args[] = L"| findstr /B /I \"lunar javaw chrome firefox cmd conhost tasklist wmiprvse powershell runtimebroker shellexperiencehost svchost useroobebroker systemsettings calculator applicationframehost steam steamwebhelper discord nvidia textinputhost security lockapp task win csrss searchindexer appvsh office mpcopy sgrm vmmem msmpeng nvtelemetry amd intel nvdisplay fontdrv lsass lsaiso services smss registry secure system idle process image name brave opera ====\"";
+	ZeroMemory(&lpCommandLine, sizeof(lpCommandLine));
+	wcscpy(lpCommandLine, windir);
+	wcsncat(lpCommandLine, cmd, wcslen(cmd));
+	wcsncat(lpCommandLine, windir, wcslen(windir));
+	wcsncat(lpCommandLine, tasklist, wcslen(tasklist));
+	wcsncat(lpCommandLine, tasklistargs, wcslen(tasklistargs));
+	if (findstr == TRUE) {
+		wcsncat(lpCommandLine, args, wcslen(args));
+	}
 	if (CreateProcessW(NULL,
 		lpCommandLine,
 		NULL,
